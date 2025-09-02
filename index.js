9982ae894774c88b4ec84085453c851865456b38
ManjirimGawali <manjirigawali39@gmail.com>
2025-09-02 23:56:31 +0530

    initializing project

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app=express();
+const port=5000;
+app.get("/",(req,res)=>{
+    // res.json({message:"Hello from Manjiri"})
+    res.send('Hello Manjiri,ypur backend is working');
+    console.log("I am getting get");
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`)
+
+})
